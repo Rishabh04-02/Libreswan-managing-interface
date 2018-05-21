ed1c900c9e09b733315cc6f45eb7ea3f33b909bc
Rishabh04-02 <rishabh0402@gmail.com>
2018-05-22 02:24:11 +0530

    admin profile created, user creation created

--- libreswan_managing_interface/settings.py
+++ libreswan_managing_interface/settings.py
@@ -75,10 +75,13 @@ WSGI_APPLICATION = 'libreswan_managing_interface.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/home/rishabh/Desktop/Libreswan-managing-interface/libreswan_managing_interface/database.cnf',
+        },
     }
 }
+
 
 
 # Password validation
--- vpn/views.py
+++ vpn/views.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("This is the vpn interface.")
