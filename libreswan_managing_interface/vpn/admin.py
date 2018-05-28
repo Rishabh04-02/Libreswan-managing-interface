21877d0869a340f4158e30435be5968cdc1f4273
Rishabh04-02 <rishabh0402@gmail.com>
2018-05-29 00:17:54 +0530

    corrections in model

--- libreswan_managing_interface/vpn/admin.py
+++ libreswan_managing_interface/vpn/admin.py
@@ -6,23 +6,40 @@ from django.contrib import admin
 # Register your models here.
 
 #For accessing the set IP
-from .models import dnsconfig
+from django.core.exceptions import ValidationError
+from django import forms
 
+from .models import dnsconfig
+"""
+#for data validation
+class dnsconfigForm(forms.model):
+
+    class Meta:
+        model=dnsconfig
+
+    def clean_right_text(self):
+        data=self.cleaned_data['right_text']
+        #Check if right_text == %any
+        if data=='%any':
+            raise ValidationError(_('Right text can not be %any'))
+
+        return data
+"""
 
 def write_to_file(modeladmin,request,queryset):
     for qs in queryset:
-        file = "/etc/ipsec.conf"    #writing data to ipsec.conf file
+        file = "/home/rishabh/Desktop/try.txt"    #writing data to ipsec.conf file
         f = open(file, 'a')  # Opening file in append mode
         f.write("conn\t"+qs.connection_name + "\n")
-        f.write("\t\t"+qs.left_text + "\n")
-        f.write("\t\t"+qs.left_subnet_text + "\n")
-        f.write("\t\t"+qs.left_next_hop + "\n")
-        f.write("\t\t"+qs.left_source_ip + "\n")
-        f.write("\t\t"+qs.right_text + "\n")
-        f.write("\t\t"+qs.right_subnet_text + "\n")
-        f.write("\t\t"+qs.right_next_hop + "\n")
-        f.write("\t\t"+qs.right_source_ip + "\n")
-        f.write("\t\t"+qs.keyring_tries + "\n")
+        f.write("\t\tleft="+qs.left_text + "\n")
+        f.write("\t\tleftsubnet="+qs.left_subnet_text + "\n")
+        f.write("\t\tleftnexthop="+qs.left_next_hop + "\n")
+        f.write("\t\tleftsourceip="+qs.left_source_ip + "\n")
+        f.write("\t\tright="+qs.right_text + "\n")
+        f.write("\t\trightsubnet="+qs.right_subnet_text + "\n")
+        f.write("\t\trightnexthop="+qs.right_next_hop + "\n")
+        f.write("\t\trightsourceip="+qs.right_source_ip + "\n")
+        f.write("\t\tkeyringtries="+qs.keyring_tries + "\n")
         f.close()
 
 
--- libreswan_managing_interface/vpn/models.py
+++ libreswan_managing_interface/vpn/models.py
@@ -7,16 +7,16 @@ from django.db import models
 
 #Model for checking when are the values stored in ipsec.conf
 class dnsconfig(models.Model):
-    connection_name = models.CharField(max_length = 20,default='')
-    left_text = models.CharField(max_length = 16,default='')
-    left_subnet_text = models.CharField(max_length=19,default='')
-    left_next_hop = models.CharField(max_length=16,default='')
-    left_source_ip = models.CharField(max_length=16,default='')
-    right_text = models.CharField(max_length=16,default='')
-    right_subnet_text = models.CharField(max_length=19, default='')
-    right_next_hop = models.CharField(max_length=16,default='')
-    right_source_ip = models.CharField(max_length=16,default='')
-    keyring_tries = models.CharField(max_length=16,default='%forever')
+    connection_name = models.CharField(max_length = 20,default='',help_text="Choices - %defaultroute OR leave blank, Reference - https://libreswan.org/man/ipsec.conf.5.html")
+    left_text = models.CharField(max_length = 16,default='',help_text="Choices - %defaultroute OR leave blank")
+    left_subnet_text = models.CharField(max_length=19,default='',help_text="Choices - %defaultroute OR leave blank")
+    left_next_hop = models.CharField(max_length=16,default='',help_text="Choices - %defaultroute OR leave blank")
+    left_source_ip = models.CharField(max_length=16,default='',help_text="Choices - %defaultroute OR leave blank")
+    right_text = models.CharField(max_length=16,default='',help_text="Choices - %defaultroute OR leave blank")
+    right_subnet_text = models.CharField(max_length=19, default='',help_text="Choices - %defaultroute OR leave blank")
+    right_next_hop = models.CharField(max_length=16,default='',help_text="Choices - %defaultroute OR leave blank")
+    right_source_ip = models.CharField(max_length=16,default='',help_text="Choices - %defaultroute OR leave blank")
+    keyring_tries = models.CharField(max_length=16,default='%forever',help_text="Choice - %forever")
 
     def __str__(self):
         return self.connection_name
